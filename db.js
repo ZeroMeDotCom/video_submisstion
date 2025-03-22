70a8c4d90f73671b5fd6feb165c5d4476f71a139
Ueki Kaoru <44609412+ZeroMeDotCom@users.noreply.github.com>
2025-03-22 16:47:47 +0800

    Add files via upload

--- db.js
+++ db.js
@@ -1,12 +1,40 @@
 const mysql = require('mysql2');
 const db = mysql.createConnection({
-	host: 'localhost',
+	host: 'mysql.railway.internal',
 	user: 'root',
-	password: 'Terry20021010!*',
-	database: 'video_app'
+	password: 'PGCnUTKmtZKMdKzUFyjmGyPwCslehUsK',
+	database: 'railway',
+	port: 26644
 });
 db.connect(err => {
 	if (err) throw err;
 	console.log('MySQL Connected!')
 });
 module.exports = db;
+
+
+
+// const mysql = require('mysql2');
+
+// const db = mysql.createPool({
+//   host: 'mysql.railway.internal',
+//   port: 3006,
+//   user: 'root',
+//   password: 'PGCnUTKmtZKMdKzUFyjmGyPwCslehUsK',
+//   database: 'railway',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// // Optional: Test the connection
+// db.getConnection((err, connection) => {
+//   if (err) {
+//     console.error('❌ MySQL Connection Error:', err);
+//   } else {
+//     console.log('✅ MySQL Connected!');
+//     connection.release();
+//   }
+// });
+
+// module.exports = db;
